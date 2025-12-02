ab5bce53f51e2a6a6ea724e7c287c330bfe0cf7b
naim <naim.baijid@gmail.com>
2025-12-03 02:23:17 +0600

    anomaly db model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,6 @@ SQLAlchemy Database Models
 
 from app.models.user import User, UserRole
 from app.models.shop import Shop, ShopManager
+from app.models.anomaly import Anomaly, AnomalyStatus, AnomalySeverity
 
-__all__ = ["User", "UserRole", "Shop", "ShopManager"]
+__all__ = ["User", "UserRole", "Shop", "ShopManager", "Anomaly", "AnomalyStatus", "AnomalySeverity"]
